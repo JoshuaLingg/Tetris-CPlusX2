b10bb1bc71c3d3f199866cdda1e3fb69d31ae876
JoshuaLing <joshualing81@gmail.com>
2023-07-04 15:11:29 +1000

    first commit

---
+++ src/grid.h
@@ -0,0 +1,26 @@
+#pragma once  //runs only once
+#include <vector>
+#include <raylib.h>
+
+//header file for declaration
+
+class Grid{
+  public:
+    Grid();
+    void Initialize();
+    void Print();
+    void Draw();
+    bool IsCellOutside(int row, int column);
+    bool isCellEmpty(int row, int column);
+    int ClearFullRows();
+    int grid[20][10];
+
+  private:
+    bool IsRowFull(int row);
+    void ClearRow(int row);
+    void MoveRowDown(int row, int numRows);
+    int numRows;
+    int numCols;
+    int cellSize;
+    std::vector<Color> colors;
+};
